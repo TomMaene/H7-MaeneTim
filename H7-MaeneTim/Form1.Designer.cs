c9f9dadc30b5a298c2d7bd7ddb4391f4955bd18c
Luna Brouckmeersch <luna.brouckmeersch@student.ppsjo.be>
2023-04-19 11:38:36 +0200

    wijziging kleur button

--- H7-MaeneTim/Form1.Designer.cs
+++ H7-MaeneTim/Form1.Designer.cs
@@ -33,12 +33,13 @@
             // 
             // button1
             // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.button1.Location = new System.Drawing.Point(254, 124);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(247, 142);
             this.button1.TabIndex = 0;
             this.button1.Text = "Maene Tim";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
